4d248dae8ab9ee60a1084d2ff5a59a6bccaa2cca
robotikitbindonesia <robotik.itbindonesia@gmail.com>
2025-10-26 22:16:45 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,7 +4,7 @@
           authDomain: "rekrutmenrobotikitbi2025.firebaseapp.com",
           databaseURL: "https://rekrutmenrobotikitbi2025-default-rtdb.asia-southeast1.firebasedatabase.app",
           projectId: "rekrutmenrobotikitbi2025",
-          storageBucket: "rekrutmenrobotikitbi2025.firebasestorage.app",
+          storageBucket: "rekrutmenrobotikitbi2025.appspot.com",
           messagingSenderId: "899366353919",
           appId: "1:899366353919:web:7372240bf986b5724ac5ca",
           measurementId: "G-6CHC1P48SF"
