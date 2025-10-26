3d168040e394eadb6982817d48548a67da01cc31
robotikitbindonesia <robotik.itbindonesia@gmail.com>
2025-10-26 20:31:02 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,18 @@
-// Firebase Configuration
-// TODO: GANTI DENGAN CONFIG FIREBASE ANDA dari Firebase Console
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+// TODO: GANTI DENGAN CONFIG FIREBASE ANDA
+        const firebaseConfig = {
+          apiKey: "AIzaSyA7Srz3fWUL4BrEkA2I6Ywsq2O9Ehhh7os",
+          authDomain: "rekrutmenrobotikitbi2025.firebaseapp.com",
+          databaseURL: "https://rekrutmenrobotikitbi2025-default-rtdb.asia-southeast1.firebasedatabase.app",
+          projectId: "rekrutmenrobotikitbi2025",
+          storageBucket: "rekrutmenrobotikitbi2025.firebasestorage.app",
+          messagingSenderId: "899366353919",
+          appId: "1:899366353919:web:7372240bf986b5724ac5ca",
+          measurementId: "G-6CHC1P48SF"
+        };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        const database = firebase.database();
 
 // Export untuk digunakan di file lain
 window.firebaseDb = database;
