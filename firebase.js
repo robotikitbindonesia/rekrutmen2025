9fd9e9c10fe1d33e7da34a016342e4a335d4187a
robotikitbindonesia <robotik.itbindonesia@gmail.com>
2025-10-26 20:29:31 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Firebase Configuration
+// TODO: GANTI DENGAN CONFIG FIREBASE ANDA dari Firebase Console
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+// Export untuk digunakan di file lain
+window.firebaseDb = database;
