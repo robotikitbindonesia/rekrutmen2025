31fb5e5ab9fda6cfb606c5cfd0638ccfefd1aa45
robotikitbindonesia <robotik.itbindonesia@gmail.com>
2025-10-26 21:21:05 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -18,7 +18,6 @@
 // window.firebaseDb = database;
 
 
-
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-analytics.js";
